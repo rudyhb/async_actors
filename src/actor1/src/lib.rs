1b664e682a3293c5ac66d7aefe51e710520bbe15
Rudy <rudyhasbun@gmail.com>
2022-05-18 20:43:15 -0500

    maybe working?

--- src/actor1/src/lib.rs
+++ src/actor1/src/lib.rs
@@ -1,20 +1,44 @@
-use ic_cdk::export::Principal;
-use ic_cdk::api::call;
 use candid::Nat;
+use ic_cdk::api::call;
+use ic_cdk::export::Principal;
 
 const ACTOR2_1: &'static str = "ryjl3-tyaaa-aaaaa-aaaba-cai";
 const ACTOR2_2: &'static str = "r7inp-6aaaa-aaaaa-aaabq-cai";
+const ACTOR2_3: &'static str = "rdmx6-jaaaa-aaaaa-aaadq-cai";
+const ACTOR2_4: &'static str = "qoctq-giaaa-aaaaa-aaaea-cai";
+const ACTOR2_5: &'static str = "qjdve-lqaaa-aaaaa-aaaeq-cai";
+const ACTOR2_6: &'static str = "qaa6y-5yaaa-aaaaa-aaafa-cai";
+const ACTOR2_7: &'static str = "qhbym-qaaaa-aaaaa-aaafq-cai";
+const ACTOR2_8: &'static str = "qsgjb-riaaa-aaaaa-aaaga-cai";
+const ACTOR2_9: &'static str = "qvhpv-4qaaa-aaaaa-aaagq-cai";
+const ACTOR2_10: &'static str = "renrk-eyaaa-aaaaa-aaada-cai";
 
 #[ic_cdk_macros::update]
 async fn run() {
     ic_cdk::println!("start actor1");
-    let principal1 = Principal::from_text(ACTOR2_1).unwrap();
-    let principal2 = Principal::from_text(ACTOR2_2).unwrap();
-    let (_response1, _response2): (Result<(), _>, Result<(),_>) = futures::future::join(
-        call::call(principal1, "run1", (Nat::from(3u64),)),
-        call::call(principal2, "run1", (Nat::from(1u64),))
-    ).await;
-    _response1.unwrap();
-    _response2.unwrap();
+    let principals: Vec<_> = [
+        ACTOR2_1, ACTOR2_2, ACTOR2_3, ACTOR2_4, ACTOR2_5, ACTOR2_6, ACTOR2_7, ACTOR2_8, ACTOR2_9,
+        ACTOR2_10,
+    ]
+    .iter()
+    .map(|p| Principal::from_text(p).unwrap())
+    .collect();
+    let mut responses: Vec<(String, Nat)> =
+        futures::future::join_all(principals.into_iter().map(|p| async move {
+            let result: Result<(Nat,), _> = call::call(p, "get_num", ()).await;
+            (p.to_string(), result.unwrap().0.clone())
+        }))
+        .await
+        .into_iter()
+        .collect();
+    responses.sort_by(|a, b| a.0.cmp(&b.0));
+    ic_cdk::println!(
+        "responses:\n{}",
+        responses
+            .into_iter()
+            .map(|r| format!("{}: {}", r.0, r.1.to_string()))
+            .collect::<Vec<String>>()
+            .join("\n")
+    );
     ic_cdk::println!("end actor1");
 }
--- src/actor2/src/lib.rs
+++ src/actor2/src/lib.rs
@@ -1,16 +1,37 @@
-use std::ops::{Mul, Rem};
-use candid::Nat;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::{Hash, Hasher};
+use std::ops::{Add, Mul, Rem};
+use once_cell::sync::Lazy;
+use parking_lot::{RwLock};
+use candid::{Nat,CandidType};
+use serde::*;
 
 #[ic_cdk_macros::update]
-fn run1(delay: Nat) {
-    ic_cdk::println!("start actor2");
+fn get_num() -> Nat {
+    let my_id = ic_cdk::api::id();
+    ic_cdk::println!("start actor2 at {}", my_id);
     let mut _counter = Nat::from(1u64);
-    let delay = delay.mul(1_000_000u64);
+    let delay = Nat::from(3u64).mul(1_000_000u64);
     let zero = Nat::from(0u64);
 
     // simulate delay
     while _counter.clone().rem(delay.clone()) != zero {
         _counter += 1;
     }
-    ic_cdk::println!("end actor2");
+    let old = STORAGE.read().value.clone();
+
+    let mut hasher = DefaultHasher::new();
+    my_id.to_string().hash(&mut hasher);
+    STORAGE.write().value = old.add(Nat::from(hasher.finish() % 100));
+
+    let ret = STORAGE.read().value.clone();
+    ic_cdk::println!("end actor2: {}", ret);
+    ret
 }
+
+#[derive(CandidType, Deserialize, Default)]
+struct Storage {
+    value: Nat,
+}
+
+static STORAGE: Lazy<RwLock<Storage>> = Lazy::new(|| RwLock::new(Storage::default()));
