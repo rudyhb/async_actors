85277873ff1c117083b45554a301ea8b690dc05c
Rudy <rudyhasbun@gmail.com>
2022-05-18 23:39:47 -0500

    .

--- src/actor2/src/lib.rs
+++ src/actor2/src/lib.rs
@@ -2,20 +2,20 @@ use candid::{CandidType, Nat};
 use serde::*;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
-use std::ops::{Mul, Rem};
+// use std::ops::{Mul, Rem};
 
 #[ic_cdk_macros::update]
 fn get_num() -> Nat {
     let my_id = ic_cdk::api::id();
     ic_cdk::println!("start actor2 at {}", my_id);
-    let mut _counter = Nat::from(1u64);
 
-    let delay = Nat::from(2u64).mul(1_000_000u64);
-    let zero = Nat::from(0u64);
-    // simulate delay
-    while _counter.clone().rem(delay.clone()) != zero {
-        _counter += 1;
-    }
+    // let mut _counter = Nat::from(1u64);
+    // let delay = Nat::from(2u64).mul(1_000_000u64);
+    // let zero = Nat::from(0u64);
+    // // simulate delay
+    // while _counter.clone().rem(delay.clone()) != zero {
+    //     _counter += 1;
+    // }
 
     let storage: &mut Storage = ic_cdk::storage::get_mut();
     let mut hasher = DefaultHasher::new();
